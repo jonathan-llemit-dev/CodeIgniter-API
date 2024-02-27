6ccb793714f3e7de00ab32502872028599bc14ae
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-27 18:47:10 +0800

    insert_api($data) added on api model

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -5,5 +5,15 @@ class Api_model extends CI_Model {
         $this->db->order_by('id', 'DESC');
         return $this->db->get('tbl_sample');
     }
+
+    function insert_api($data){
+        $this->db->insert('tbl_sample', $data);
+        
+        if($this->db->affected_rows() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 	
 }
