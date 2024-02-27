0102e505913c999008cd9e68f20d0ba77de37931
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-27 18:17:27 +0800

    insert() added to api controller

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -13,5 +13,31 @@ class Api extends CI_Controller {
         $data = $this->api_model->fetch_all();
         echo json_encode($data->result_array());
     }
+
+    function insert(){
+        $this->form_validation->set_rules("first_name", "First Name", "required");
+        $this->form_validation->set_rules("last_name", "Last Name", "required");
+        $array = array();
+
+        if($this->form_validation->run()){
+            $data = array(
+                'first_name' => trim($this->input->post('first_name')),
+                'last_name'  => trim($this->input->post('last_name'))
+            );
+
+            $this->api_model->insert_api($data);
+            $array = array(
+                'success'  => true
+            );
+        }else{
+            $array = array(
+                'error'    => true,
+                'first_name_error' => form_error('first_name'),
+                'last_name_error' => form_error('last_name')
+            );
+        }
+        
+        echo json_encode($array, true);
+    }
 	
 }
