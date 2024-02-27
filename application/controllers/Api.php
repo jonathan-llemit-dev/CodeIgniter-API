ca5bf725af668dacb02ab5adff053bd4ca29f8de
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-27 17:33:43 +0800

    initial commit

---
+++ application/controllers/Api.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	
+}
---
+++ application/controllers/Test_api.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test_api extends CI_Controller {
+
+	
+}
