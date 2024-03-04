dc55cceaeb57e9919d19c8f08766bc1c503ebb63
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-03-04 18:24:23 +0800

    now API is working!

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -6,97 +6,15 @@ class Api extends CI_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->model('Api_model');
-        $this->load->library('form_validation');
     }
 
-    function index(){
+    public function index(){
         $data = $this->Api_model->get_data();
-        return json_encode($data->result_array());
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
     }
 
-    function insert(){
-        $this->form_validation->set_rules("first_name", "First Name", "required");
-        $this->form_validation->set_rules("last_name", "Last Name", "required");
-        $array = array();
-
-        if($this->form_validation->run()){
-            $data = array(
-                'first_name' => trim($this->input->post('first_name')),
-                'last_name'  => trim($this->input->post('last_name'))
-            );
-
-            $this->api_model->insert_api($data);
-            $array = array(
-                'success'  => true
-            );
-        }else{
-            $array = array(
-                'error'    => true,
-                'first_name_error' => form_error('first_name'),
-                'last_name_error' => form_error('last_name')
-            );
-        }
-
-        echo json_encode($array, true);
-    }
-
-    function fetch_single(){
-        if($this->input->post('id')){
-            $data = $this->api_model->fetch_single_user($this->input->post('id'));
-
-            foreach($data as $row)
-            {
-                $output['first_name'] = $row["first_name"];
-                $output['last_name'] = $row["last_name"];
-            }
-
-            echo json_encode($output);
-        }
-    }
-
-    function update(){
-        $this->form_validation->set_rules("first_name", "First Name", "required");
-        $this->form_validation->set_rules("last_name", "Last Name", "required");
-        $array = array();
-
-        if($this->form_validation->run()){
-            $data = array(
-                'first_name' => trim($this->input->post('first_name')),
-                'last_name'  => trim($this->input->post('last_name'))
-            );
-
-            $this->api_model->update_api($this->input->post('id'), $data);
-            
-            $array = array(
-                'success'  => true
-            );
-        }else{
-            $array = array(
-                'error'    => true,
-                'first_name_error' => form_error('first_name'),
-                'last_name_error' => form_error('last_name')
-            );
-        }
-
-        echo json_encode($array, true);
-    }
-
-    function delete(){
-        if($this->input->post('id')){
-
-            if($this->api_model->delete_single_user($this->input->post('id')))
-            {
-                $array = array(
-                'success' => true
-                );
-            }else{
-                $array = array(
-                'error' => true
-                );
-            }
-
-            echo json_encode($array);
-        }
-    }
-	
 }
+
+ 
