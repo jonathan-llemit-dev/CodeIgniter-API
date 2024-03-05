497c14a5b96a93897f98d0824a53871ba5e5957e
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-03-05 13:21:18 +0800

    update_data function on model added

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -16,4 +16,9 @@ class Api_model extends CI_Model {
         return $this->db->insert('tbl_sample', $data);
     }
 
+    public function update_data($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update('tbl_sample', $data);
+    }
+
 }
