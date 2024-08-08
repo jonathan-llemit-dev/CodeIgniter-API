cf3dfd71b51fe76b8604753a047df7efbd249ea1
Jonathan Llemit <jonathan@clearmindai.com>
2024-08-08 23:32:37 +0800

    added email and age

--- dump/dump.sql
+++ dump/dump.sql
@@ -9,5 +9,7 @@ CREATE TABLE IF NOT EXISTS `tbl_sample` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(50) NOT NULL,
     `last_name` VARCHAR(50) NOT NULL,
+    `age` INT NOT NULL,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
     PRIMARY KEY (`id`)
 );
