672e757d674e7a8ec7eb1348dfe0072dff0d4842
Jonathan Llemit <jonathan@clearmindai.com>
2024-08-09 09:25:16 +0800

    base url modified

--- application/config/config.php
+++ application/config/config.php
@@ -1,29 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-/*
-|--------------------------------------------------------------------------
-| Base Site URL
-|--------------------------------------------------------------------------
-|
-| URL to your CodeIgniter root. Typically this will be your base URL,
-| WITH a trailing slash:
-|
-|	http://example.com/
-|
-| WARNING: You MUST set this value!
-|
-| If it is not set, then CodeIgniter will try to guess the protocol and
-| path to your installation, but due to security concerns the hostname will
-| be set to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
-| The auto-detection mechanism exists only for convenience during
-| development and MUST NOT be used in production!
-|
-| If you need to allow multiple domains, remember that this file is still
-| a PHP script and you can easily do that on your own.
-|
-*/
-$config['base_url'] = 'http://localhost:8000';
+// local
+// $config['base_url'] = 'http://localhost:8001';
+// UAT
+$config['base_url'] = 'http://192.168.160.120:8001';
 
 /*
 |--------------------------------------------------------------------------
